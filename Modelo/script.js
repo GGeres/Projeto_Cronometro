867e20997fd4a7d115065d78386edb3d73a9d9d1
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-03-28 14:23:36 -0300

    Novos Dados

---
+++ Modelo/script.js
@@ -0,0 +1,9 @@
+let seg = document.getElementById('seg')
+let mile = document.getElementById('mile')
+let start = document.getElementById('start_buttom')
+let stop = document.getElementById('stop_buttom')
+let reset = document.getElementById('reset_buttom')
+
+function start(){
+
+}
